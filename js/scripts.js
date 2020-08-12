14caf7efadb495e4e9ba5ec38bdc124d19003fc6
Gerzera <66367261+Gerzera@users.noreply.github.com>
2020-08-12 15:08:15 +0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+$(".info-1").click(function() {
+    $(".call-1").css("display", "block");
+});
+$(".info-2").click(function() {
+    $(".call-2").css("display", "block");
+});
+
+$(".info-3").click(function() {
+    $(".call-3").css("display", "block");
+});
+
+$(".info-4").click(function() {
+    $(".call-4").css("display", "block");
+});
+
+
+$(".close-1").click(function() {
+    $(".call-1").css("display", "none");
+});
+
+$(".close-2").click(function() {
+    $(".call-2").css("display", "none");
+});
+
+$(".close-3").click(function() {
+    $(".call-3").css("display", "none");
+});
+
+$(".close-4").click(function() {
+    $(".call-4").css("display", "none");
+});
